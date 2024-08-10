571aaa3b7dcbc35d487e9338c628e74620e7ebb4
김태관 <iamtk@neowiz.com>
2024-08-10 19:01:33 +0900

    first commit

---
+++ app/info/contact/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h1 className="text-7xl">Contact Page</h1>
+      <Link className="text-xl text-blue-500 inline-block mt-8" href="/">
+        back home
+      </Link>
+    </div>
+  );
+};
+export default ContactPage;
---
+++ app/tours/page.tsx
@@ -0,0 +1,33 @@
+import test from "node:test";
+
+const url = "https://www.course-api.com/react-tours-project";
+
+type Tour = {
+  id: string;
+  name: string;
+  info: string;
+  image: string;
+  price: string;
+};
+
+const fetchTours = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  const response = await fetch(url);
+  const data: Tour[] = await response.json();
+  return data;
+};
+
+const ToursPage = async () => {
+  // const response = await fetch(url);
+  // const data: Tour[] = await response.json();
+  const data = await fetchTours();
+  return (
+    <section>
+      <h1 className="text-3xl mb-4">Tours</h1>
+      {data.map(({ id, name }) => {
+        return <h2 key={id}>{name}</h2>;
+      })}
+    </section>
+  );
+};
+export default ToursPage;
