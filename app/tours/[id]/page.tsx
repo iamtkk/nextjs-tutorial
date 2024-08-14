f5ca06d2b10224dc104a0ff4ef9be0032200b59a
김태관 <iamtk@neowiz.com>
2024-08-14 17:50:59 +0900

    Image Components

--- app/tours/[id]/page.tsx
+++ app/tours/[id]/page.tsx
@@ -1,7 +1,44 @@
+import mapsImg from "@/images/maps.jpg";
+import { Irish_Grover } from "next/font/google";
+import Image from "next/image";
+const url = "https://www.course-api.com/images/tours/tour-1.jpeg";
+console.log("mapsImg : ", mapsImg);
+
 const page = ({ params }: { params: { id: string } }) => {
   return (
     <div>
       <h1 className="text-4xl">ID: {params.id}</h1>
+      <section className="flex gap-x-4 mt-4">
+        {/* <div> */}
+        {/* origin image */}
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        {/* <img src="/maps.jpg" alt="maps" />
+          <h2>origin image</h2>
+        </div> */}
+        {/* local image */}
+        <div>
+          <Image
+            className="w-48 h-48 object-cover rounded"
+            src={mapsImg}
+            priority
+            alt="maps"
+            width={192}
+            height={192}
+          />
+          <h2>local image</h2>
+        </div>
+        {/* remote image */}
+        <div>
+          <Image
+            className="w-48 h-48 object-cover rounded"
+            src={url}
+            alt="tour"
+            width={192}
+            height={192}
+          />
+          <h2>remote image</h2>
+        </div>
+      </section>
     </div>
   );
 };
--- app/tours/page.tsx
+++ app/tours/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const url = "https://www.course-api.com/react-tours-project";
@@ -11,7 +12,7 @@ type Tour = {
 };
 
 const fetchTours = async () => {
-  await new Promise((resolve) => setTimeout(resolve, 1500));
+  await new Promise((resolve) => setTimeout(resolve, 200));
   const response = await fetch(url);
   const data: Tour[] = await response.json();
   return data;
@@ -24,13 +25,29 @@ const ToursPage = async () => {
   return (
     <section>
       <h1 className="text-3xl mb-4">Tours</h1>
-      {data.map(({ id, name }) => {
-        return (
-          <Link className="hover:text-blue-500" key={id} href={`/tours/${id}`}>
-            <h2>{name}</h2>
-          </Link>
-        );
-      })}
+      <div className="grid md:grid-cols-2 gap-8">
+        {data.map(({ id, name, image }) => {
+          return (
+            <Link
+              className="hover:text-blue-500"
+              key={id}
+              href={`/tours/${id}`}
+            >
+              <div className="relative h-48 mb-2">
+                <Image
+                  className="object-cover rounded"
+                  src={image}
+                  alt={name}
+                  fill
+                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                  priority
+                />
+              </div>
+              <h2>{name}</h2>
+            </Link>
+          );
+        })}
+      </div>
     </section>
   );
 };
