39bc7bc7794c9c94e6e5724be9c671e331b75a1f
김태관 <iamtk@neowiz.com>
2024-08-15 20:37:31 +0900

    POST route handler 정의

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,5 +1,5 @@
 import { saveUser, fetchUsers } from "@/utils/actions";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (req: NextRequest) => {
   // const { searchParams } = new URL(req.url);
@@ -8,6 +8,13 @@ export const GET = async (req: NextRequest) => {
   console.log("req.nextUrl : ", req.nextUrl.searchParams.get("id"));
 
   const users = await fetchUsers();
-  // return Response.json({ users });
-  return Response.redirect(new URL("/", req.nextUrl));
+  return Response.json({ users });
+  // return NextResponse.redirect(new URL("/", req.nextUrl));
+};
+
+export const POST = async (req: Request) => {
+  const user = await req.json();
+  const newUser = { ...user, id: Date.now().toString() };
+  await saveUser(newUser);
+  return Response.json({ msg: "user created" });
 };
